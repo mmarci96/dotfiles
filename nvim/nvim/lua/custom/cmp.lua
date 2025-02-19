1188e5abf57bb7d8574618ccd9836e4bbfcfa2aa
mmarci96 <sarosdimarci@gmail.com>
2025-02-19 03:04:03 +0100

    tailwind lsp worked now

---
+++ nvim/nvim/lua/custom/cmp.lua
@@ -0,0 +1,37 @@
+local cmp = require "cmp"
+
+cmp.setup {
+    completion = {
+        autocomplete = { require("cmp.types").cmp.TriggerEvent.TextChanged },
+        delay = 200,
+        completeopt = "menu,menuone,noselect",
+    },
+    mapping = {
+        ["<CR>"] = cmp.mapping.confirm { select = true },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.confirm { select = true }
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+    },
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    sources = {
+        { name = "ts_ls" },
+        { name = "luasnip" },
+        { name = "nvim_lsp" },
+        { name = "path",       trigger_characters = { "/", "./" } },
+        { name = "buffer" },
+        { name = "spell",      option = { enable_in_comment = false } },                 -- Spell-check
+        { name = "cmp-react" }
+    },
+    preselect = cmp.PreselectMode.Item,
+}
